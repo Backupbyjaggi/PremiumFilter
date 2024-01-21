40e82261793ec777fa8b9cd8ab2d94abdf2f82fe
Backupbyjaggi <109049575+Backupbyjaggi@users.noreply.github.com>
2024-01-21 10:49:16 +0530

    Update yt_dl.py

--- plugins/Tamilans Extra Mods/yt_dl.py
+++ plugins/Tamilans Extra Mods/yt_dl.py
@@ -43,7 +43,7 @@ async def song(client, message):
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
 
-        cap = "**BY›› [Tᴀᴍɪʟᴀɴ BᴏᴛsZ](https://t.me/Tamilan_BotsZ)**"
+        cap = "**BY›› [Tᴀᴍɪʟᴀɴ BᴏᴛsZ](https://t.me/BackupByJaggi)**"
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
